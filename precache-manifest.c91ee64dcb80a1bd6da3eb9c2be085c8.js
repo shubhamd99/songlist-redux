9040f873233fcfd51297c0a253d052e944e38388
Shubham Dhage <shubhamdhage930@gmail.com>
2019-04-27 14:02:36 +0530

    Updates

---
+++ precache-manifest.c91ee64dcb80a1bd6da3eb9c2be085c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6057172bcc5d2721b0a5b69897b6a9aa",
+    "url": "/songlist-redux/index.html"
+  },
+  {
+    "revision": "70b4023c76ab79eec238",
+    "url": "/songlist-redux/static/css/main.bdec3951.chunk.css"
+  },
+  {
+    "revision": "924cfad3472eab555a92",
+    "url": "/songlist-redux/static/js/2.5191c8ec.chunk.js"
+  },
+  {
+    "revision": "70b4023c76ab79eec238",
+    "url": "/songlist-redux/static/js/main.de7d770f.chunk.js"
+  },
+  {
+    "revision": "6a10124c20563851bcaf",
+    "url": "/songlist-redux/static/js/runtime~main.823cd37f.js"
+  },
+  {
+    "revision": "d8862555b6f0660cd39e5e2e252c5cae",
+    "url": "/songlist-redux/static/media/cover.d8862555.png"
+  }
+]);
